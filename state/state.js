c50258b37014e6c9b8867ded6c18ca248cea706c
Xahyx7 <mohdzahikzafar@gmail.com>
2026-01-23 19:17:06 +0530

    Update state.js

--- state/state.js
+++ state/state.js
@@ -2,6 +2,29 @@
 
 export const appState = {
   subjects: [],
+  goals: [
+  {
+    id: "g1",
+    subjectId: "math",
+    subjectName: "maths",
+    topic: "Trigonometry",
+    type: "time",
+    target: 25,
+    progress: 10,
+    completed: false
+  },
+  {
+    id: "g2",
+    subjectId: "math",
+    subjectName: "maths",
+    topic: "Algebra",
+    type: "units",
+    target: 2,
+    progress: 1,
+    completed: false
+  }
+],
+
   sessions: [],
 
   goals: {
