9f69ed39cbf3cec4d5da890ae4bbdb9a3289549b
Xahyx7 <mohdzahikzafar@gmail.com>
2026-01-23 11:43:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 import { loadState } from "./state/persistence.js";
 import { renderDashboard } from "./ui/dashboard.js";
 import { setupAddSubjectModal } from "./ui/addSubjectModal.js";
+import { setupFocusTimer } from "./ui/focusTimer.js";
 
 loadState();
 renderDashboard();
 setupAddSubjectModal();
+setupFocusTimer();
