5381d8b8047bc8ba83e5354b5e52499e9699b15d
Xahyx7 <mohdzahikzafar@gmail.com>
2026-01-23 20:25:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 // app.js
 
 import { renderDashboard } from "./ui/dashboard.js";
+import { setupFocusTimer } from "./ui/focusTimer.js";
+import { setupFocusMode } from "./ui/focusMode.js";
+import { setupAddSubject } from "./ui/addSubject.js";
+import { renderTodayRing } from "./ui/todayRing.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // Initial render
   renderDashboard();
+  renderTodayRing();
+
+  // Setup interactions
+  setupFocusTimer();
+  setupFocusMode();
+  setupAddSubject();
 });
