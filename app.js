ef16c088002fff6392156701275777424c605693
Xahyx7 <mohdzahikzafar@gmail.com>
2026-01-23 20:01:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 // app.js
 
-import { loadState } from "./state/persistence.js";
 import { renderDashboard } from "./ui/dashboard.js";
-import { renderTodayRing } from "./ui/todayRing.js";
-import { setupAddSubjectModal } from "./ui/addSubjectModal.js";
-import { setupFocusTimer } from "./ui/focusTimer.js";
-import { setupFocusMode } from "./ui/focusMode.js";
 
-loadState();
-renderDashboard();
-renderTodayRing();
-
-
-setupAddSubjectModal();
-setupFocusTimer();
-setupFocusMode();
+document.addEventListener("DOMContentLoaded", () => {
+  renderDashboard();
+});
