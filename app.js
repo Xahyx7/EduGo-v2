231b39bb84b2b24c0caa5c9bd853fa2b77eb75a0
Xahyx7 <mohdzahikzafar@gmail.com>
2026-01-25 15:50:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ import { renderDashboard } from "./ui/dashboard.js";
 import { setupAddSubject } from "./ui/addSubject.js";
 import { setupFocusTimer } from "./ui/focusTimer.js";
 import { renderTodayRing } from "./ui/todayRing.js";
+import { setupGoals } from "./ui/goals.js";
+import { renderGoals } from "./ui/renderGoals.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Initial UI render
   renderDashboard();
+  renderGoals();
   renderTodayRing();
 
-  // Setup interactions
   setupAddSubject();
+  setupGoals();
   setupFocusTimer();
 });
